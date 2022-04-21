6b7ad46cf6672a9f86046d9d9f37c09146cfbe39
sumit-kumar <sumitkumar740100@gmail.com>
2022-04-21 18:59:50 -0400

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_133145) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_225747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,10 +46,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_133145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "publish"
-    t.integer "user_id"
     t.string "name"
     t.text "description"
     t.string "price"
+    t.integer "user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
